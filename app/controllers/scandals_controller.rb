43d1901e77bea8e316c6eb3684cb03beab4fc27d
yuto <nekopurin06300630@yahoo.co.jp>
2020-06-20 13:12:31 +0900

    env change

--- app/controllers/scandals_controller.rb
+++ app/controllers/scandals_controller.rb
@@ -3,7 +3,6 @@ class ScandalsController < ApplicationController
 
 def client
   @client ||= Line::Bot::Client.new { |config|
-    config.channel_id = ENV["LINE_CHANNEL_ID"]
     config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
     config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
   }
